4cce5d7f2110908e30f3daab079fb4d5010c145e
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-10-31 16:51:27 +0200

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for digit1 in range(0, 9)
-  for digit2 in range(digit1 + 1, 9)
+for digit1 in range(0, 9):
+  for digit2 in range(digit1 + 1, 9):
     print("{}{}".format(digit1, digit2))
   print("{:d}".format(89))
