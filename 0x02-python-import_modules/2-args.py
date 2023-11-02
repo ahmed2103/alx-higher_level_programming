84bd04c4c23693625102eca407c038d9fb0af07f
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-02 20:41:52 +0200

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-import sys
-
-if __name__ == "__main":
+if __name__ == "__main__":
+    import sys
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
