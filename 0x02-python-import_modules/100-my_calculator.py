92a89f52f75581f28bc3e403e3c389e579911884
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-02 21:33:52 +0200

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
-
-if __name__ == "__main":
+if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv, exit
-
     if len(argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
     a = int(argv[1])
-    operator = argv[2]
     b = int(argv[3])
-
-    if operator == "+":
-        result = add(a, b)
-    elif operator == "-":
-        result = sub(a, b)
-    elif operator == "*":
-        result = mul(a, b)
-    elif operator == "/":
-        result = div(a, b)
+    if argv[2] == '+':
+        val = add(a, b)
+    elif argv[2] == '-':
+        val = sub(a, b)
+    elif argv[2] == '*':
+        val = mul(a, b)
+    elif argv[2] == '/':
+        val = div(a, b)
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
+        print("Unknown operator. Only: +, -, * and / available")
         exit(1)
-
-    print("{} {} {} = {}".format(a, operator, b, result))
+    print("{:d} {} {:d} = {:d}".format(a, argv[2], b, val))
