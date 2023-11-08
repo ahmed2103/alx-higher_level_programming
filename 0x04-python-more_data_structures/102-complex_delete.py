3c440cd16506bf0c4f73f4fddbcafce0b1623704
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-08 21:59:52 +0200

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,4 +1,9 @@
-def complex_delete(a_dictionary, value):
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if(my_list):
+        return(sum(x*y for x, y in my_list) / sum(y for x, y in my_list))
+    else:
+        return(0)def complex_delete(a_dictionary, value):
     if value:
         for key, val in list(a_dictionary.items()):
             if val is value:
