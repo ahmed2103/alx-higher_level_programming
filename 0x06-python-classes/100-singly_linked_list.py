fb6073a64232268ad8f44ba9cd4db60107ad42af
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-21 20:37:50 +0200

    Update 100-singly_linked_list.py

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,42 +1,63 @@
 #!/usr/bin/python3
-""" Module providing a 'Node' class for a singly-linked list
-"""
-
 
 class Node:
-    """ Definition of a singly-linked list node
+    """ Definition of a node of a singly linked list
     """
     def __init__(self, data, next_node=None):
-        """ Instantiate a node
+        """ Instantiate a node with data and optional next_node
         """
         self.data = data
         self.next_node = next_node
 
+    @property
+    def data(self):
+        """ Retrieve the data stored in a node
+        """
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        """ Set the data stored in a node
+        """
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        """ Retrieve the next node
+        """
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        """ Set the next node
+        """
+        if value is not None and not isinstance(value, Node):
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
 
 class SinglyLinkedList:
-    """ Definition of a singly-linked list
+    """ Definition of a singly linked list
     """
     def __init__(self):
-        """ Instantiate a singly-linked list
+        """ Instantiate a singly linked list with head as None
         """
         self.head = None
 
     def __str__(self):
-        """ Generate a visual representation of a list
+        """ Print the entire list in stdout
         """
-        if self.head is None:
-            return "Empty list"
-
         result = ""
         current = self.head
         while current is not None:
-            result += str(current.data) + " -> "
+            result += str(current.data) + "\n"
             current = current.next_node
-        result += "None"
         return result
 
     def sorted_insert(self, value):
-        """ Insert a Node into a list sorted in ascending order
+        """ Insert a new Node into the correct sorted position in the list (increasing order)
         """
         new_node = Node(value)
 
