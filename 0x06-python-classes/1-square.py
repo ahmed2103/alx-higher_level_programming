f1bf750027b4d56adc880cdcedf8f51096940529
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-21 17:45:17 +0200

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class Square that represents a square"""
+
+class Square:
+    """class Square that represents a square"""
+    __size = None
+
+    def __init__(self, size):
+        """Instation with size"""
+        self.__size = size
