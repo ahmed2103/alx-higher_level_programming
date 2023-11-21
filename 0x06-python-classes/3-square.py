35738db53d17d228fd6283271295b2ae180ee257
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-21 20:12:06 +0200

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
+"""class Square that defines a square"""
+
+
 class Square:
     """class Square that defines a square"""
-    __size = None
 
     def __init__(self, size=0):
-        """Initialization with optional size"""
-        if type(size) is int:
-            if size >= 0:
-                self.__size = size
+        """Instantiation with optional size"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Property to retrieve the size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Property setter to set the size"""
+        if type(value) is int:
+            if value >= 0:
+                self.__size = value
             else:
                 raise ValueError("Size must be >= 0")
         else:
             raise TypeError("Size must be an integer")
 
     def area(self):
-        """Calculate the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
+        """Public instance method that returns the current square area"""
         return self.__size * self.__size
