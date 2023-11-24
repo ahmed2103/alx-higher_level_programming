5af988df8afa08ede3528c618c18164e57f668a4
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-24 16:45:57 +0200

    Create 3-say_my_name.py

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Defines a function to print a person's name'''
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints the given first and last name.
+
+    Args:
+    - first_name (str): The first name of the person.
+    - last_name (str, optional): The last name of the person. Defaults to an empty string.
+
+    Raises:
+    - TypeError: If either first_name or last_name is not a string.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    if not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    print("My name is {} {}".format(first_name, last_name))
