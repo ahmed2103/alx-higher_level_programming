edb23096ef03e119f719682ba22f1701631ef815
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-24 17:05:43 +0200

    Create 6-max_integer_test.py

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,36 @@
+==============================
+How to use 6-max_integer.py
+==============================
+
+This module contains unit tests for the `max_integer` function using Python's `unittest` framework.
+
+Test Cases
+----------
+
+`TestMaxInteger` class:
+    This class contains multiple test cases to verify the functionality of the `max_integer` function.
+
+    1. `test_one`: Test case with a list of integers in ascending order.
+    2. `test_two`: Test case with a list of integers in descending order.
+    3. `test_three`: Test case with a single negative integer.
+    4. `test_four`: Test case with an empty list.
+    5. `test_five`: Test case with a list containing a single element.
+    6. `test_six`: Test case with a list containing multiple negative integers.
+    7. `test_seven`: Test case with a list containing duplicates.
+    8. `test_eight`: Test case with a list containing all similar integers.
+    9. `test_nine`: Test case with a list of integers and one float.
+    10. `test_ten`: Test case with a list of integers in random order.
+
+Usage Example:
+--------------
+
+To run the test cases, execute the file `6-max_integer.py`:
+
+    $ python3 6-max_integer.py
+
+Upon execution, the `unittest` framework runs all defined test cases and reports any failures or errors.
+
+Note:
+-----
+
+This test file demonstrates various scenarios to validate the correctness of the `max_integer` function using different input lists.
