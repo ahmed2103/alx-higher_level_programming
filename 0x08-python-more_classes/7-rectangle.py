6473acba14be2b3d9357545989c799b5ad45ddb9
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-11-28 15:01:26 +0200

    Create 7-rectangle.py

---
+++ 0x08-python-more_classes/7-rectangle.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+
+"""rectangle class"""
+
+
+class Rectangle:
+    """This class represents a rectangle object"""
+    print_symbol = '#'
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """initializes a rectangle object"""
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Gets width"""
+        return self._width
+
+    @property
+    def height(self):
+        """Gets height"""
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        """Sets height"""
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self._height = value
+
+    @width.setter
+    def width(self, value):
+        """Sets width"""
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self._width = value
+
+    def area(self):
+        """returns area of the object"""
+        return self.width * self.height
+
+    def perimeter(self):
+        """returns area of the rectangle"""
+        if self.height and self.width:
+            return 2 * (self.width + self.height)
+        return 0
+
+    def __str__(self):
+        """informal string representation of the rectangle"""
+        if self.perimeter():
+            return '\n'.join("{}".format(self.print_symbol) * self.width
+                             for _ in range(self.height))
+        else:
+            return ''
+
+    def __repr__(self):
+        """formal representation of the rectangle"""
+        return "Rectangle({}, {})".format(self.width, self.height)
