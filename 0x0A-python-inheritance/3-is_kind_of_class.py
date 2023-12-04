84993ff60306dd69323fe89784668d94ec3820c4
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-12-04 23:45:12 +0200

    Update 3-is_kind_of_class.py

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,3 +1,7 @@
-def is_kind_of_class(obj, a_class):
-    return isinstance(obj, a_class)
+#!/usr/bin/python3
+"""Module implementing function to verify class membership"""
 
+
+def is_kind_of_class(obj, a_class):
+    """Checks if obj is an instance of or inherits from a_class"""
+    return isinstance(obj, a_class)
