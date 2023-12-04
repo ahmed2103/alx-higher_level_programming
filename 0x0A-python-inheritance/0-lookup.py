8c5a255ebdb85187132c12a959f17506230aeb3b
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-12-04 23:28:27 +0200

    Update 0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+"""Module implementing function to retrieve object attributes"""
+
 def lookup(obj):
+    """Retrieve a list of attributes and methods associated with obj."""
     return dir(obj)
