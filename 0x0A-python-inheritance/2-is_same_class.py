ef525bab0ce2b67cbfa7b9aa007f59ec5567c954
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-12-04 23:43:22 +0200

    Update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,2 +1,7 @@
-def is_same_class(obj, a_class):
+#!/usr/bin/python3
+"""Module defining function to test class membership"""
+
+
+def is_same_class(obj, a_class):   
+    """Returns True if object is an instance and not a subclass of a_class"""
     return type(obj) == a_class
