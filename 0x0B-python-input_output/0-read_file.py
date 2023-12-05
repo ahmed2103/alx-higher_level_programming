38725d047fc7ead6c4eacb51ab5dc44dae27e972
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-12-05 17:48:11 +0200

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,12 @@
-def read_file(filename=""):
-    """Read the contents of a file in utf-8 encoding and print to stdout.
+def append_write(filename="", text=""):
+    """Append `text` to `filename` if it exists, or create `filename` if it doesn't.
 
     Args:
-        filename (str): The file to open and read.
+        filename (str): The file to write to or create.
+        text (str): The text to append in utf-8 encoding.
+
+    Returns:
+        int: Number of characters written to the file.
     """
-    with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read())
+    with open(filename, 'a', encoding='utf-8') as file:
+        return file.write(text)
