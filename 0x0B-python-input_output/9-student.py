c142c56d9aec3df813c6a9f3dea5256b87744d4e
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-12-05 23:03:32 +0200

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """module implement class student has a method to seralize itself"""
+
 class student:
     """class represent a student"""
     def __init__(self, first_name, last_name, age):
+        
         """student constructor to make instances"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+        
     def to_json(self):
         """returns json dict of attributes in read only view"""
         return self.__dict__
