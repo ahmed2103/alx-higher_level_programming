bec36166f625e8cb402ca2fbec27a6ba2dc09dd7
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-12-05 17:53:14 +0200

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,15 @@
-#!usr/bin/python3
-def append_write(filename="", text=""):
-    with open(filename, "a", encoding="utf-8") as f:
-        f.write(text)
-        return len(text)
+#!/usr/bin/python3
+"""Module to append `text` to `filename`"""
 
+def append_write(filename="", text=""):
+    """Append `text` to `filename` if it exists, or create `filename` if it doesn't.
+
+    Args:
+        filename (str): The file to write to or create.
+        text (str): The text to append in utf-8 encoding.
+
+    Returns:
+        int: Number of characters written to the file.
+    """
+    with open(filename, 'a', encoding='utf-8') as file:
+        return file.write(text)
