0a57f2b6ffc840097c26b82b93a9fd83e5e3c41f
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-12-05 17:47:17 +0200

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
-#!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, "r", encoding = "utf-8") as f:
-        print(f.read())
+    """Read the contents of a file in utf-8 encoding and print to stdout.
+
+    Args:
+        filename (str): The file to open and read.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        print(file.read())
