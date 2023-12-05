909a1222014381e948109d16e2a6557f6973b9a1
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2023-12-05 17:45:36 +0200

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,12 @@
-#!/usr/bin/python3
 def write_file(filename="", text=""):
-    with open(filename, 'r', encoding = 'utf-8') as f:
-        f.write(text)
-        return len(text)
- 
+    """Write `text` to `filename` and return the number of characters written.
+
+    Args:
+        filename (str): The file to write to.
+        text (str): The text to write into `filename`.
+
+    Returns:
+        int: Number of characters written to the file.
+    """
+    with open(filename, 'w', encoding='utf8') as file:
+        return file.write(text)
