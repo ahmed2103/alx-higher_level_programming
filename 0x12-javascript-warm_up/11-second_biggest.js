4d9e5257f81eebacf36ffdcbf3f38a20f58a086d
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-02-13 03:45:29 +0200

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-
-function secondBiggest(arr) {
-  if (arr.length <= 2) {
-    return 0;
-  }
-  const sortedArr = arr.sort((a, b) => b - a);
-  return sortedArr[1];
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
 }
-
-const args = process.argv.slice(2).map(Number);
-console.log(secondBiggest(args));
