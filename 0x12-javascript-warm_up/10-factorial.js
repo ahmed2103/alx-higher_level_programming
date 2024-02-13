809d67b034b401f89a9b8f7cf8f1fd803a0ff7c2
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-02-13 03:40:34 +0200

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 function factorial(n) {
-  if (isNaN(n)) {
-    return 1;
+  if (isNaN(n) || n < 0) {
+    return NaN;
   }
   if (n === 0) {
     return 1;
@@ -10,4 +10,4 @@ function factorial(n) {
   return n * factorial(n - 1);
 }
 
-console.log(factorial(parseInt(process.argv[2])));
+console.log(factorial(Number(process.argv[2])));
