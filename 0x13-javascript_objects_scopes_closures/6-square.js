c3573328995ed7f4f217d6cc791aed95d266faeb
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-02-14 21:51:04 +0200

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,22 +1,20 @@
 #!/usr/bin/node
 
-const SquareC = require('./5-square')
+const squareC = require('./5-square');
 
-class square extends SquareC {
-	charPrint(c) {
-		if (c === undefined) {
-			c = 'x';
-		}
-		for (let i = 0; i < this.height; i++) {
-			let s = '';
-			for (let j = 0; j < this.width; j++) {
-				s += c;
-			}
-			console.log(s);
-		}
-	}
+class Square extends squareC {
+  charPrint(c) {
+    if (c === undefined) {
+      c = 'x';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
 }
 
-module.ecports = Square;
-
-			
+module.exports = Square;
