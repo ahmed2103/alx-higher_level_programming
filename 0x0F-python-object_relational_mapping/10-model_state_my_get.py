340bd9cf6aba2e131d4c54869ab21c505d2641e5
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-02-19 00:27:52 +0200

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
+""""
+script that lists all State objects that contain the letter a
+from the database `hbtn_0e_6_usa`
 """
- script that prints the State object with the name passed as
- argument from the database `hbtn_0e_6_usa`
-"""
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_state import Base, State
+
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3])
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
-    session = sessionmaker(bind=engine)
+    Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter(state.name == (argv[4], ))
-    try:
-        print(state[0].id)
-    except IndexError:
-        print("Not found")
+    for state in session.query(State).filter(State.name.like('%a%')):
+        print(state.id, state.name, sep=": ")
