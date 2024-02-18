80718988170838a4d11606d7ee3c930441a0c263
kali <bekoahmed20@gmail.com>
2024-02-19 00:14:51 +0200

    in shaaa allah

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Script that takes in an argument and displays all values
+ in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], db=argv[3], port=3306)
+    curs = db.cursor()
+    curs.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+                 .format(argv[4]))
+    for row in curs.fetchall():
+        print(row)
+    curs.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""""
+script that lists all State objects that contain the letter a
+from the database `hbtn_0e_6_usa`
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).filter(State.name.like('%a%')):
+        print(instance.id, instance.name, sep=": ")
