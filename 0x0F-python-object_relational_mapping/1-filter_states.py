d8233d1b2620c25f886d7ddbff695514d285bc31
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-02-18 19:14:19 +0200

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,13 +5,12 @@ from the database `hbtn_0e_0_usa`
 """
 if __name__ == "__main__":
     import sys
-    import MYSQLdb
+    import MySQLdb
 
     """establish connection to database"""
-    db = MYSQLdb.connect(host = 'localhost'user=sys.argv[1], passwd=sys, db=sys.argv[2],
-                         port = 3306)
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     curs = db.cursor()
-    curs.execute("SELECT * FROM states WHERE name LIKE '%N' ORDER BY id ASC")
+    curs.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = curs.fetchall()
     for row in rows:
         print(row)
