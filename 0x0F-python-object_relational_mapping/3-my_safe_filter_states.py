1c528149e4dec07075a0aff2e475bec0eb6ca79f
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-02-18 19:22:14 +0200

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -3,14 +3,17 @@
 modified script to prevent MYSQL injection when attempting to use malicious query
 """
 from sys import argv
-import MYSQLdb
+import MySQLdb
+
 if __name__ == "__main__":
-    db = MYSQLdb.connect(user=argv[1], passwd=argv[2], host="localhost"
-                         , db = argv[3])
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], host="localhost", db=argv[3])
     curs = db.cursor()
-    curs.execute("SELECT * FROM states WHERE name like %s BY id in ASC",
-                 argv[4])
-    for row in curs.fetchall:
+    
+    # Using parameterized query to prevent SQL injection
+    curs.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", (argv[4],))
+    
+    for row in curs.fetchall():
         print(row)
+    
     curs.close()
     db.close()
