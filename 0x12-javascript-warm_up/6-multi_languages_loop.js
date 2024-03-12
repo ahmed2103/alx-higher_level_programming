95e80ad7991acc0ee3ebb421b494f4211ed09e0e
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-03-12 04:47:30 +0200

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
 
-line = ['C is fun',
+const line = ['C is fun',
   'Python is cool',
   'JavaScript is amazing'];
+
 for (let i = 0; i < line.length; i++) {
   console.log(line[i]);
 }
