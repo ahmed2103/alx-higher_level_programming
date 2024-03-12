535ef37cf3ca2ebf0e6084c130a4bb19d89e28d5
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-03-12 04:48:45 +0200

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,7 +5,7 @@ if (x === undefined || isNaN(x)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < x; i++) {
-    row = '';
+    let row = '';
     for (let j = 0; j < x; j++) {
       row += 'X';
     }
