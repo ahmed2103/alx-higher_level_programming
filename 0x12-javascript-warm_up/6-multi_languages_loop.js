0903214f440e2411493ca8a723a8964ca7eb10ac
kali <kali@kali.kali>
2024-03-13 16:46:57 +0200

    a

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+line = ['C is fun',
+  'Python is cool',
+  'JavaScript is amazing'];
+for (let i = 0; i < line.length; i++) {
+  console.log(line[i]);
+}
---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const x = process.argv[2];
+if (x === undefined || isNaN(x)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < x; i++) {
+    row = '';
+    for (let j = 0; j < x; j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
