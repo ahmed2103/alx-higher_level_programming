54826944ae8e976f344659f431b67c62594040c1
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-03-16 10:54:08 +0200

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                            .format(argv[1],argv[2],argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    session = Session(engine)
+    session = Session()
     for instance in (session.query(State.name, City.id, City.name)
             .filter(State.id == City.state_id)):
         print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
