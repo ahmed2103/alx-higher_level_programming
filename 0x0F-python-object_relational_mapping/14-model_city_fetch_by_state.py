f310fd033954b527e9dc7ffa3bdbb22d03a70b79
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-03-16 10:58:38 +0200

    Update 14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-
-"""script that that prints all City objects from the database hbtn_0e_14_usa"""
-from model_state import Base, State
+""" 
+Prints the State object with the name passed as argument from the database
+"""
 from sys import argv
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(argv[1],argv[2],argv[3]))
+                           .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     for instance in (session.query(State.name, City.id, City.name)
-            .filter(State.id == City.state_id)):
+                     .filter(State.id == City.state_id)):
         print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
-
