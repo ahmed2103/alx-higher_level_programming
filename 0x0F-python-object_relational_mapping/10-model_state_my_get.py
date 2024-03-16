4e6f61ed9b4c9f29af5911c17181b01a9c16b433
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-03-16 10:42:48 +0200

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-
+""" prints the State object with the name passed as argument from the database
 """
-script that lists all State objects that contain the letter `a`
- from the database `hbtn_0e_6_usa`
-"""
-from sys import argv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+import sys
 from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
-                           .format(argv[1],argv[2],argv[3]))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    session = Session(engine)
-    states = session.query(State).filter(State.name.like('%a%'))
-    for state in states:
-        print(state.id, state.name, sep=": ")
-    session.close()
+    session = Session()
+    instance = session.query(State).filter(State.name == (sys.argv[4],))
+    try:
+        print(instance[0].id)
+    except IndexError:
+        print("Not found")
