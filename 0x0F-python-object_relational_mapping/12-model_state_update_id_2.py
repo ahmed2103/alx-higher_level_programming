8e2e70978cc77d5fbfb15cd2c8fb634f56f815ca
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-03-16 10:50:41 +0200

    Update 12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     state = session.query(State).filter_by(id = 2).first()
-    session.name = 'New Mexico'
+    state.name = 'New Mexico'
     session.commit()
     session.close()
