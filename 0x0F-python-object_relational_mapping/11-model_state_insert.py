2af5aa46a38625e61fb2b0679471f7a37efea303
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-03-16 10:47:10 +0200

    Update 11-model_state_insert.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                            .format(argv[1], argv[2], argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    session = Session(engine)
+    session = Session()
     new = State(name="Louisiana")
     session.add(new)
     new_state = session.query(State).filter_by(name="Louisiana").first()
