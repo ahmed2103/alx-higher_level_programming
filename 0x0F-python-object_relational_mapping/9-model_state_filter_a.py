eb80c9759541ec1b023dd64fff9f375cc36cc7d3
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-03-16 10:27:43 +0200

    Update 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
                            .format(argv[1],argv[2],argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-    session = Session(engine)
+    session = Session()
     states = session.query(State).filter(State.name.like('%a%'))
     for state in states:
         print(state.id, state.name, sep=": ")
