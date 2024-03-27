2d257e76392a8691f2f9c2fa27f022b7f45dade8
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-03-27 22:13:47 +0200

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -10,7 +10,7 @@ def find_peak(list_of_integers):
     left = 0
     right = len(list_of_integers) - 1
     while left< right:
-        mid = (lo + rifht) // 2
+        mid = (left + rifht) // 2
         if list_of_integers[mid] < list_of_integers[mid + 1]:
             left = mid + 1
         else:
