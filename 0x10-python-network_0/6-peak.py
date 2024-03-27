ebc8f40eb990c8d28c0fc85d308b669340e4dbfe
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-03-27 22:19:59 +0200

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 
-"""Script that contain a function to get max int of an unsorted list"""
+"""Script that contains a function to find a peak in an unsorted list"""
 
 def find_peak(list_of_integers):
-    """function to get max int inspired from binary search not actually"""
+    """Function to find a peak in a list of integers, inspired by binary search"""
 
-   if list_of_integers is None or list_of_integers == []:
+    if list_of_integers is None or list_of_integers == []:
         return None
+    
     left = 0
     right = len(list_of_integers) - 1
+    
     while left < right:
         mid = (left + right) // 2
+        
         if list_of_integers[mid] < list_of_integers[mid + 1]:
             left = mid + 1
         else:
             right = mid
+    
     return list_of_integers[left]
