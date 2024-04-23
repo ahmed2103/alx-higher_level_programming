91dd4d9b028ec77b3a88c093f4016fa16e27295b
Hasona <hasona@kali.kali>
2024-04-23 20:58:11 +0200

    test

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const fs = require("fs");
+const fileName = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(fileName, content, "utf-8", err => {
+    if (err) return console.log(err);
+})
